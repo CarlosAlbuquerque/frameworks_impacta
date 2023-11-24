0e686a08c1dd4ca01e8767f81144376b9a1d67dc
CarlosAlbuquerque <carloshalbuquerque2@gmail.com>
2023-11-24 16:56:55 -0300

    feat: adding postgress and event entity

---
+++ tickets-manager/src/events/dto/create-event.dto.ts
@@ -0,0 +1,8 @@
+export class CreateEventDto {
+  name: string;
+  capacity: number;
+  location: string;
+  date: Date;
+  description: string;
+  imageUrl: string;
+}
--- tickets-manager/src/user/user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { userLevel } from './dto/create-user.dto';
-
-export class UserEntity {
-  id: string;
-  name: string;
-  email: string;
-  level: userLevel;
-  password: string;
-}
